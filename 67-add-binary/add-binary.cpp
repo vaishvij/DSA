939330e58c9a951c2932fc5f2fc70adc78b1e146
Vaishvi Jain <129960758+vaishvij@users.noreply.github.com>
2026-01-12 14:21:33 +0530

    Time: 2 ms (27.37%) | Memory: 10 MB (25.01%) - LeetSync

--- 67-add-binary/add-binary.cpp
+++ 67-add-binary/add-binary.cpp
@@ -5,21 +5,17 @@ public:
         string ans = "";
         int i = a.length()-1;
         int j = b.length()-1;
-        char c='0';
-
-        while(i>=0 || j>=0 || c=='1')
+        int carry = 0;
+        
+        while(i>=0 || j>=0)
         {
-            char x = (i>=0) ? a[i] : '0';
-            char y = (j>=0) ? b[j] : '0';
-
-            if(c=='0' && x=='0' && y=='0') ans+='0';
-            else if(c=='1' && x=='0' && y=='0') {ans+='1'; c='0';}
-            else if((c=='0' && x=='0' && y=='1') || (c=='0' && x=='1' && y=='0')) ans+='1';
-            else if((c=='1' && x=='0' && y=='1') || (c=='1' && x=='1' && y=='0')) {ans+='0'; c='1';}
-            else if(c=='0' && x=='1' && y=='1') {ans+='0'; c='1';}
-            else if(c=='1' && x=='1' && y=='1') {ans+='1'; c='1';}
-            i--; j--;
+            int sum = carry;
+            if(i >= 0) sum = sum + (a[i--] - '0');
+            if(j >= 0) sum = sum + (b[j--] - '0');
+            carry = sum>1 ? 1 : 0;
+            ans += to_string(sum%2);
         }
+        if(carry == 1) ans += to_string(carry);
         reverse(ans.begin(),ans.end());
         return ans;
     }
