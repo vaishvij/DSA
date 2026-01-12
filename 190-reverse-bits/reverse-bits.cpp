5845a6a7f537df0064b2daf880411807584dd524
Vaishvi Jain <129960758+vaishvij@users.noreply.github.com>
2026-01-12 14:23:49 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (14.91%) - LeetSync

--- 190-reverse-bits/reverse-bits.cpp
+++ 190-reverse-bits/reverse-bits.cpp
@@ -2,20 +2,12 @@ class Solution {
 public:
     int reverseBits(int n) 
     {
-        string org="";
+        int ans=0;
         for(int i=0; i<32; i++)
         {
-            if(n%2 == 1) org+='1';
-            else org+='0';
-            n = n>>1;
+            ans = (ans<<1) | (n&1);
+            n >>= 1;
         }
-        int len = org.length();
-        int rev=0, p=1;
-        for(int i=len-1; i>0; i--)
-        {
-            if(org[i] == '1') rev=rev+p;
-            p = p<<1;
-        }
-        return rev;
+        return ans;
     }
 };
