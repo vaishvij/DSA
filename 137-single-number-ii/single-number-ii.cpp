78c52b4cebe669cdedfa9b8ca377bd84f69f123a
Vaishvi Jain <129960758+vaishvij@users.noreply.github.com>
2026-02-02 00:41:23 +0530

    Time: 6 ms (5.47%) | Memory: 14.1 MB (10.71%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) 
+    {
+        map<int,int> freq;
+        for(int i=0; i<nums.size(); i++)
+        {
+            freq[nums[i]]++;
+        }
+        for(auto i : freq)
+        {
+            if(i.second == 1)
+            return i.first;
+        }
+        return -1;
+    }
+};
